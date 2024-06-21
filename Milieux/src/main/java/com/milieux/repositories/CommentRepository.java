6be421ad5a3c33483dce5d9dc8d7ba9376482f40
R1B3n <R1B3n@localhost>
2024-06-21 14:43:08 +0600

    change name

--- Milieux/src/main/java/com/milieux/repositories/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.milieux.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.milieux.models.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
--- Milieux/src/main/java/com/milieux/repositories/StoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.milieux.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.milieux.models.Story;
-
-public interface StoryRepository extends JpaRepository<Story, Long> {
-
-}
