4cd0674ddbd902c273198aad748fab5cca2231f9
K2LU <rigansust@gmail.com>
2024-08-26 03:07:21 +0600

    contextAPI

--- backend/src/main/java/com/milieux/ecomm/review/ReviewController.java
+++ backend/src/main/java/com/milieux/ecomm/review/ReviewController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,22 +26,22 @@ public class ReviewController {
     }
 
     @GetMapping("/find/{id}")
-    List <Review> findById(Integer id) {
+    List <Review> findById(@PathVariable Integer id) {
         return repository.findByProductId(id);
     }
 
     @PostMapping("/create")
-    void create(Review review) {
+    void create(@RequestBody Review review) {
         repository.create(review);
     }
 
     @PostMapping("/update/{id}")
-    void update(Review review, Integer id) {
+    void update(@RequestBody Review review, @PathVariable Integer id) {
         repository.update(review, id);
     }
 
     @DeleteMapping("/delete/{id}")
-    void delete(Integer id) {
+    void delete(@PathVariable Integer id) {
         repository.delete(id);
     }
 }
