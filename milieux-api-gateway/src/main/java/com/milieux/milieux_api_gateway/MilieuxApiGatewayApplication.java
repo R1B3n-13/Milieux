24f550b0fb5a1e6eecd4a3ea93b6c84838e5001e
R1B3n-13 <nebir2002@gmail.com>
2024-10-05 21:18:49 +0600

    change cors setting

--- milieux-api-gateway/src/main/java/com/milieux/milieux_api_gateway/MilieuxApiGatewayApplication.java
+++ milieux-api-gateway/src/main/java/com/milieux/milieux_api_gateway/MilieuxApiGatewayApplication.java
@@ -8,19 +8,29 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class MilieuxApiGatewayApplication {
 
 	public static void main(String[] args) {
-		
+
 		Dotenv dotenv = Dotenv.load();
 
 		System.setProperty("SPRING_CLOUD_GATEWAY_ROUTES_0_URI",
 				dotenv.get("SPRING_CLOUD_GATEWAY_ROUTES_0_URI", "http://localhost:8082"));
+
 		System.setProperty("SPRING_CLOUD_GATEWAY_ROUTES_0_PREDICATES_0",
 				dotenv.get("SPRING_CLOUD_GATEWAY_ROUTES_0_PREDICATES_0", "/ecomm/**"));
+
 		System.setProperty("SPRING_CLOUD_GATEWAY_ROUTES_1_URI",
 				dotenv.get("SPRING_CLOUD_GATEWAY_ROUTES_1_URI", "http://localhost:8081"));
+
 		System.setProperty("SPRING_CLOUD_GATEWAY_ROUTES_1_PREDICATES_0",
 				dotenv.get("SPRING_CLOUD_GATEWAY_ROUTES_1_PREDICATES_0", "/**"));
-		System.setProperty("SPRING_CLOUD_GATEWAY_GLOBALCORS_CORS_CONFIGURATIONS___ALLOWEDORIGINS",
-				dotenv.get("SPRING_CLOUD_GATEWAY_GLOBALCORS_CORS_CONFIGURATIONS___ALLOWEDORIGINS", "*"));
+
+		System.setProperty("SPRING_CLOUD_GATEWAY_GLOBALCORS_CORS_CONFIGURATIONS___ALLOWEDORIGINS_0",
+				dotenv.get("SPRING_CLOUD_GATEWAY_GLOBALCORS_CORS_CONFIGURATIONS___ALLOWEDORIGINS_0",
+						"http://localhost:3000/"));
+
+		System.setProperty("SPRING_CLOUD_GATEWAY_GLOBALCORS_CORS_CONFIGURATIONS___ALLOWEDORIGINS_1",
+				dotenv.get("SPRING_CLOUD_GATEWAY_GLOBALCORS_CORS_CONFIGURATIONS___ALLOWEDORIGINS_1",
+						"http://localhost:3001/"));
+
 		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT", "8080"));
 
 		SpringApplication.run(MilieuxApiGatewayApplication.class, args);
